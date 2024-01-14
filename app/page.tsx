13074c5376f9aafc5dc0fd715ad44b20802f9066
Omar Ahmed <omarabozeid.pro@gmail.com>
2024-01-14 17:07:51 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,10 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className={styles.shape} />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
-            <strong>Welcome to Acme.</strong> This is the example for Financial Dashboard{' '}
-            
+            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <a href="" className="text-blue-500">
+              Test Project for a financial dashboard 
+            </a>
             , brought to you by Vercel.
           </p>
           <Link
