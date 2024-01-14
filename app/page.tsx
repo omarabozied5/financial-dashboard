aeab110b971ed3e0804dfbfaf56dc8df177164d2
Omar Ahmed <omarabozeid.pro@gmail.com>
2024-01-14 22:56:12 +0200

    some update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ import Link from 'next/link';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
+
 export default function Page() {
+  
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
